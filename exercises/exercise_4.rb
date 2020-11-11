aab5d3807a129f4ef7f70874a822096d207c9032
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-11-11 04:28:13 +0000

    Accessed some data, connected employees and store tables, and added seed data to employees table

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,21 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Store.create(name: "Surrey",annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler",annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown",annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+puts Store.count
+
+@mens_stores = Store.where(mens_apparel: true)
+
+# puts @mens_stores.inspect
+
+@mens_stores.each do | element | 
+  puts "This store is called: #{element.name} and it has a revenue of: #{element.annual_revenue}"
+end
+
+@womens_stores = Store.where("womens_apparel = true AND annual_revenue < 1000000")
+@womens_stores.each do | element |
+  puts "This store is called: #{element.name} and it has a revenue of:  #{element.annual_revenue}"
+end
